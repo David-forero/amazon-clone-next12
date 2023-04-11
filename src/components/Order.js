60817e3618c80cd6330add66fecccfe70cbb963e
David Forero <54685136+David-forero@users.noreply.github.com>
2023-04-11 13:45:08 -0400

    adding i on map

--- src/components/Order.js
+++ src/components/Order.js
@@ -33,8 +33,8 @@ const Order = ({ id, amount, amountShipping, items, timestamp, images }) => {
 
             <div className='p-5 sm:p-10'>
                 <div className='flex space-x-6 overflow-x-auto'>
-                    {images?.map((image) => (
-                        <img src={image} alt="" className='h-20 object-contain sm:h-32' />
+                    {images?.map((image, i) => (
+                        <img src={image} alt="items" key={i} className='h-20 object-contain sm:h-32' />
                     ))}
                 </div>
             </div>
